f2d99d14c84ab3a3215df587ee681b01cc55c22a
Matthew Denaburg <matthew.denaburg@veracross.com>
2025-03-08 16:51:16 -0500

    Revert "convert to typescript (#11)" (#13)
    This reverts commit a85272f5a11b101e35fcb0fbe6575ebe54aee6d5.

--- docs/js/angle.js
+++ docs/js/angle.js
@@ -1,24 +1,37 @@
 /**
- * Represents an angle
+ * @class Angle - represents an angle.
  */
 class Angle {
-    /**
-     * @param angle The angle, in degrees.
-     */
-    constructor(angle) {
-        this.angle = Math.abs(angle) % 360;
-    }
-    get degrees() {
-        return this.angle;
-    }
-    /**
-     * @returns The angle, in radians.
-     */
-    get radians() {
-        return this.angle * Math.PI / 180;
-    }
-    compare(angle) {
-        return this.degrees - angle.degrees;
-    }
+  /**
+   * @private
+   * @type {int} - The angle, in degrees.
+   */
+  #angle;
+
+  /**
+   * @param {number} angle - The angle, in degrees.
+   */
+  constructor(angle) {
+    this.#angle = Math.abs(angle) % 360;
+  };
+
+  /**
+   * @returns {number} - The angle, in degrees.
+   */
+  get degrees() {
+    return this.#angle;
+  };
+
+  /**
+   * @returns {number} - The angle, in radians.
+   */
+  get radians() {
+    return this.#angle * Math.PI / 180;
+  };
+
+  compare(angle) {
+    return this.degrees - angle.degrees;
+  };
 }
+
 export default Angle;
